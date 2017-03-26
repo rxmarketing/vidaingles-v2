d459b6f145d553f242d18574c6df3fdf1735afb1
ricomx <ricomxmarketing@gmail.com>
2017-03-26 12:03:46 -0500

    Removed.

--- inc/db_vidainglescore_connn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$db_conx = mysqli_connect("localhost", "vidaingl_ricardo", "51503060pEt0", "vidaingl_vidaingles");
-if(mysqli_connect_errno()){
-	echo "Lo siento, no pude conectarme a la base de datos.";
-	exit();
-}
-// Change character set to utf8
-mysqli_set_charset($db_conx,"utf8");
-?>
